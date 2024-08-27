56f19200759a263b9338d339777e498e21668dab
AminaSalik <aminasalik012@gmail.com>
2024-08-27 21:52:35 +0100

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+   
+   
+  
+    function sub(a, b) {
+         return a - b; 
+    }
+  
+
+    console.log (sub (5, 3)) ;
+    
+    });
+    
